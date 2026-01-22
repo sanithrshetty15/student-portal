e60f9b205374053d7ccc1079a57670faa69f9ae6
sanith <sanithrshetty15@gmail.com>
2026-01-22 16:00:41 +0530

    define port and fix railway listen

--- backend/server.js
+++ backend/server.js
@@ -108,6 +108,7 @@ app.post("/api/students/login", async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
 
 // ----------------- Start server -----------------
 app.listen(PORT, "0.0.0.0", () => {
