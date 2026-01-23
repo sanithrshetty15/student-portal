399943770ee16ee7e1cc75eb0a8f7a392c549009
sanith <sanithrshetty15@gmail.com>
2026-01-23 16:13:01 +0530

    adding backend connection to server.js

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.use(express.json()); // parse JSON body
 
 // ----------------- MongoDB connection -----------------
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch(err => console.error("MongoDB connection error ❌", err));
 
 
 
